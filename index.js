31108ebf624cba401f7e2afc23cd4d23a6a31010
maryfarner18 <maryfarner@gmail.com>
2019-11-02 00:56:28 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objs){
+let found = objs.find(function(obj){
+    return obj.result === "W"
+  })
+
+  return found != undefined ? found.year : undefined
+}
